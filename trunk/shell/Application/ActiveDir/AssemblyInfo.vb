7917a0767af10d0694cc282a9c61f6e99e879fc3
sqlnut <sqlnut@9c72f8d2-1b61-11de-a108-cd2f117ce590>
2009-04-05 14:34:13 +0000

    Original check-in of roboshell executable source.

---
+++ trunk/shell/Application/ActiveDir/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ActiveDir")> 
+<Assembly: AssemblyDescription("User & User Group functions")> 
+<Assembly: AssemblyCompany("Tolbeam Pty Limited")> 
+<Assembly: AssemblyProduct("")> 
+<Assembly: AssemblyCopyright("Russell Hansen")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: CLSCompliant(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("E059F5B6-9E33-48C9-AA19-EB9255062A80")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: ComVisibleAttribute(False)> 
