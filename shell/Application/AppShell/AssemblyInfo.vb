5babedebcc8f85db6ec68091bf97be43fd9d1b40
sqlnut <sqlnut@9c72f8d2-1b61-11de-a108-cd2f117ce590>
2010-01-30 11:18:15 +0000

    AppShell: Re-initialising combo box selects the first item in the list rather than clearing.

--- shell/Application/AppShell/AssemblyInfo.vb
+++ shell/Application/AppShell/AssemblyInfo.vb
@@ -31,7 +31,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("1.7.4.*")> 
+<Assembly: AssemblyVersion("1.7.5.*")> 
 
 <Assembly: ComVisibleAttribute(False)> 
 <Assembly: NeutralResourcesLanguageAttribute("en-AU")> 
